d787eece3e6e07dd3e843850cd92cdba82ec4f42
jzquinzon <jzquinzon@gmail.com>
2019-03-26 20:47:17 -0700

    Implemented Mobile first CSS

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,7 +3,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 
 
@@ -11,7 +12,7 @@ cssImport = require('postcss-import');
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars,nested, autoprefixer]))
+        .pipe(postcss([cssImport,mixins, cssvars,nested, autoprefixer]))
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
